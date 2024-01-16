332871275b5226eb13f0f8d02eb1032baa0b0009
bhargavnene1234 <129619027+bhargavnene1234@users.noreply.github.com>
2024-01-17 00:29:10 +0530

    Delete src directory

--- src/main/java/com/example/AnimeApplication/AnimeList/repository/ListItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.AnimeApplication.AnimeList.repository;
-
-import com.example.AnimeApplication.AnimeList.model.ListItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ListItemRepository extends JpaRepository<ListItem, Long> {
-}
